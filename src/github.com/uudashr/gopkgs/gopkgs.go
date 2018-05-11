98b4743ae2a580d9d599bace2967b8b7f7eb4cd2
zyz <yanzongnet@163.com>
2018-05-11 22:29:46 +0800

    新增文件

---
+++ src/github.com/uudashr/gopkgs/gopkgs.go
@@ -0,0 +1,123 @@
+package gopkgs
+
+import (
+	"go/build"
+	"go/parser"
+	"go/token"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/MichaelTJones/walk"
+)
+
+// Pkg hold the information of the package.
+type Pkg struct {
+	Dir        string // directory containing package sources
+	ImportPath string // import path of package in dir
+	Name       string // package name
+}
+
+// Options for retrieve packages.
+type Options struct {
+	WorkDir  string // Will return importable package under WorkDir. Any vendor dependencies outside the WorkDir will be ignored.
+	NoVendor bool   // Will not retrieve vendor dependencies, except inside WorkDir (if specified)
+}
+
+// Packages available to import.
+func Packages(opts Options) (map[string]*Pkg, error) {
+	fset := token.NewFileSet()
+
+	var pkgsMu sync.Mutex
+	pkgs := make(map[string]*Pkg)
+
+	workDir := opts.WorkDir
+	if workDir != "" && !filepath.IsAbs(workDir) {
+		wd, err := filepath.Abs(workDir)
+		if err != nil {
+			return nil, err
+		}
+
+		workDir = wd
+	}
+
+	for _, srcDir := range build.Default.SrcDirs() {
+		err := walk.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				if os.IsPermission(err) {
+					return nil
+				}
+				return err
+			}
+
+			// Ignore files begin with "_", "." "_test.go" and directory named "testdata"
+			// see: https://golang.org/cmd/go/#hdr-Description_of_package_lists
+
+			name := info.Name()
+			pathDir := filepath.Dir(path)
+			if info.IsDir() {
+				if name[0] == '.' || name[0] == '_' || name == "testdata" || name == "node_modules" {
+					return walk.SkipDir
+				}
+
+				if name == "vendor" {
+					if workDir != "" {
+						if !visibleVendor(workDir, pathDir) {
+							return walk.SkipDir
+						}
+
+						return nil
+					}
+
+					if opts.NoVendor {
+						return walk.SkipDir
+					}
+				}
+
+				return nil
+			}
+
+			if name[0] == '.' || !strings.HasSuffix(name, ".go") || strings.HasSuffix(name, "_test.go") {
+				return nil
+			}
+
+			if pathDir == srcDir {
+				// Cannot put files on $GOPATH/src or $GOROOT/src.
+				return nil
+			}
+
+			filename := path
+
+			src, err := parser.ParseFile(fset, filename, nil, parser.PackageClauseOnly)
+			if err != nil {
+				// skip unparseable go file
+				return nil
+			}
+
+			pkgDir := pathDir
+			pkgName := src.Name.Name
+			if pkgName == "main" {
+				// skip main package
+				return nil
+			}
+
+			pkgsMu.Lock()
+			if _, ok := pkgs[pkgDir]; !ok {
+				pkgs[pkgDir] = &Pkg{
+					Name:       pkgName,
+					ImportPath: filepath.ToSlash(pkgDir[len(srcDir)+len("/"):]),
+					Dir:        pkgDir,
+				}
+			}
+			pkgsMu.Unlock()
+			return nil
+		})
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return pkgs, nil
+}
